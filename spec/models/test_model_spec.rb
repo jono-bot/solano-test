ec46c9a2625185e2629e50f4cd1cc267efc654c2
Jonathan Maddison <jmaddi@gmail.com>
2015-04-13 16:41:25 +1000

    Adding tests

---
+++ spec/models/test_model_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe TestModel, type: :model do
+  it 'should be successful' do
+    expect true
+  end
+  it 'should be succesfull again' do
+    expect true
+  end
+end
